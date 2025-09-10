0b6d93025eb238e3beb53028b65342a19ba1c6d9
Konstantin Láska <konstantin.laska@icloud.com>
2025-09-10 08:51:59 +0200

    Create Buttons.ino

---
+++ SW/Simple/Buttons/Buttons.ino
@@ -0,0 +1,45 @@
+/* Buttons test for LaskaKit ESPink-4.2
+ * 
+ * Board:   LaskaKit ESPink-4.2   https://www.laskakit.cz/laskakit-espink-42-esp32-e-paper-pcb-antenna
+ *
+ * 
+ * Email:podpora@laskakit.cz
+ * Web:laskakit.cz
+ */
+ 
+ // constants won't change. They're used here to set pin numbers:
+const int buttonUP    = 39;
+const int buttonPUSH  = 40;
+const int buttonDOWN  = 41;
+
+// variables will change:
+int buttonStateUP   = 0;
+int buttonStatePUSH = 0;
+int buttonStateDOWN = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonUP, INPUT);
+  pinMode(buttonPUSH, INPUT);
+  pinMode(buttonDOWN, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonStateUP = digitalRead(buttonUP);
+  buttonStatePUSH = digitalRead(buttonPUSH);
+  buttonStateDOWN = digitalRead(buttonDOWN);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonStateUP == LOW) {
+    Serial.println("Button UP pressed");
+  } 
+  if (buttonStatePUSH == LOW) {
+    Serial.println("Button PUSH pressed");
+  } 
+  if (buttonStateDOWN == LOW) {
+    Serial.println("Button DOWN pressed");
+  } 
+}
